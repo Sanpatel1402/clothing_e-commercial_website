ef5fa5b60d1d19a2e6ed82b26f6b65b3f440b688
Sanpatel1402 <sankethirani1402@gmail.com>
2023-07-20 18:47:56 +0530

    change

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-wzo8exan6itu*hf-w@)wc(!=4^4&9(cbal(_ob#ej8^w_07n__
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = []
+ALLOWED_HOST = ['3.87.240.43']
+HTTP_HOST = ['3.87.240.43:8000']
 # Application definition
 
 INSTALLED_APPS = [
